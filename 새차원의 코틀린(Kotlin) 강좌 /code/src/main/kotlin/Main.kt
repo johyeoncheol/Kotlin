e59450d6a8290e604c001897a8deaf039fb24509
johyeoncheol <qweas46@naver.com>
2022-04-17 15:33:26 +0900

    (feat:hyeoncheol) 클래스 init 연습

--- 새차원의 코틀린(Kotlin) 강좌 /code/src/main/kotlin/Main.kt
+++ 새차원의 코틀린(Kotlin) 강좌 /code/src/main/kotlin/Main.kt
@@ -5,5 +5,14 @@ fun main(args: Array<String>) {
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 
+    var obj = Customer("kotlin");
+    println(obj);
+
     test();
+}
+
+class Customer(name : String){
+    init{
+        println("이름 : $name");
+    }
 }
